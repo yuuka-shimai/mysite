c46ac2799bc1b53fa3a93f519b67ac31e63dbcf1
yuuka-shimai <yshimai@adobe.com>
2025-09-24 21:49:21 +0900

    add single image block

--- blocks/single-image/single-image.js
+++ blocks/single-image/single-image.js
@@ -8,21 +8,19 @@ export default function decorate(block) {
   // Get the image from the row
   const picture = row.querySelector('picture');
   const img = row.querySelector('img');
-  
   if (img && picture) {
     // Create optimized picture with multiple breakpoints for responsive design
     const optimizedPicture = createOptimizedPicture(
-      img.src, 
-      img.alt, 
-      false, 
+      img.src,
+      img.alt,
+      false,
       [
         { media: '(min-width: 1200px)', width: '1200' },
         { media: '(min-width: 900px)', width: '900' },
         { media: '(min-width: 600px)', width: '600' },
-        { width: '400' }
-      ]
+        { width: '400' },
+      ],
     );
-    
     // Replace the original picture with the optimized one
     picture.replaceWith(optimizedPicture);
   }
